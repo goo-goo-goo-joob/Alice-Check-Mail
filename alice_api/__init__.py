be6f797db57a86c8817f337d267a8703fd8b60dd
Unknown <andrsolo21@gmail.com>
2020-06-26 16:30:55 +0300

    baseline

--- alice_api/__init__.py
+++ alice_api/__init__.py
@@ -5,28 +5,41 @@ from enum import Enum, auto
 from flask import Flask
 from flask import request
 
-
-
 app = Flask(__name__)
 
-AGREE = ['Проверь почту', 'Хочу', 'Давай']
+AGREE = ['хочу', 'давай', 'да']
 DISAGREE = ['нет']
 
 
-class States(Enum):
-    START = auto()
-    CHECK = auto()
-    AUTH = auto()  # 0
-    NoMAILS = auto()  # 1
-    OneMAIL = auto()  # 2
-    OneSENDER = auto()  # 3
-    ManySENDERS = auto()  # 4
-    SmallMAIL = auto()  # 5
-    LargeMAIL = auto()  # 6
-    ContMAIL = auto()  # 9
-    NoMoreMAIL = auto()  # 7
-    AnyMoreMAIL = auto()  # 8
-    AnyHELP = auto()  # 10
+# class States(Enum):
+#     START = auto()
+#     CHECK = auto()
+#     AUTH = auto()  # 0
+#     NoMAILS = auto()  # 1
+#     OneMAIL = auto()  # 2
+#     OneSENDER = auto()  # 3
+#     ManySENDERS = auto()  # 4
+#     SmallMAIL = auto()  # 5
+#     LargeMAIL = auto()  # 6
+#     ContMAIL = auto()  # 9
+#     NoMoreMAIL = auto()  # 7
+#     AnyMoreMAIL = auto()  # 8
+#     AnyHELP = auto()  # 10
+
+class States():
+    START = 11
+    CHECK = 12
+    AUTH = 0  # 0
+    NoMAILS = 1  # 1
+    OneMAIL = 2 # 2
+    OneSENDER = 3 # 3
+    ManySENDERS = 4  # 4
+    SmallMAIL = 5 # 5
+    LargeMAIL = 6  # 6
+    ContMAIL = 9 # 9
+    NoMoreMAIL = 7  # 7
+    AnyMoreMAIL = 8  # 8
+    AnyHELP = 10 # 10
 
 
 class UserRecord:
@@ -77,7 +90,6 @@ def handler_check(req, res):
     res['response']['text'] = text
 
 
-
 def choose_check(req, res):
     if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in AGREE:
         return handler_read(req, res)
@@ -111,13 +123,15 @@ def main():
             "text": ""
         }
     }
-    user = storage.get(request.json['session']['user_id'])
-    if user.state == States.START:
-        choose_start(request.json, response)
-    elif user.state == States.CHECK:
-        choose_check(request.json, response)
-    else:
-        choose_start(request.json, response)
+    #user = storage.get(request.json['session']['user_id'])
+    # if user.state == States.START:
+    #     choose_start(request.json, response)
+    # elif user.state == States.CHECK:
+    #     choose_check(request.json, response)
+    # else:
+    #     choose_start(request.json, response)
+
+    main_handler(request.json, response)
 
     return json.dumps(
         response,
@@ -127,8 +141,7 @@ def main():
 
 
 def main_handler(req, res):
-
-    #TODO: Проверка на запрос выхода и помощи
+    # TODO: Проверка на запрос выхода и помощи
 
     if 'state' not in req:
         start_handler(req, res)
@@ -142,7 +155,7 @@ def main_handler(req, res):
 
     if curState == States.OneMAIL:
         if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in AGREE:
-            read_message(req, res)
+            prep_read_message(req, res)
             return
 
         if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in DISAGREE:
@@ -150,31 +163,58 @@ def main_handler(req, res):
             return
 
         do_not_understand(req, res)
+        return
 
     if curState == States.OneSENDER:
 
-        if 'request' in req and 'nlu' in req['request'] and 'tokens' in req['request']['nlu']:
-            numMessge = req['request']['nlu']['tokens'][0]
-
+        numMessge = get_number(req)
+        if numMessge:
             do_one_mail(req, res)
             return
 
         do_not_understand(req, res)
+        return
 
     if curState == States.ManySENDERS:
 
-        if 'request' in req and 'nlu' in req['request'] and 'tokens' in req['request']['nlu']:
-            numMessge = req['request']['nlu']['tokens'][0]
-
-            do_one_mail(req, res)
+        numSender = get_number(req)
+        if numSender:
+            do_one_sender(req, res)
             return
+
+        do_not_understand(req, res)
+        return
+
+    if curState == States.LargeMAIL:
+
+        if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in AGREE:
+            prep_read_message(req, res, cont=True)
+            return
+
+        if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in DISAGREE:
+            other_mails(req, res)
+            return
+
+        do_not_understand(req, res)
+        return
+
+    if curState == States.AnyMoreMAIL:
+        if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in AGREE:
+            start_handler(req, res)
+            return
+
+        if 'request' in req and 'original_utterance' in req['request'] and req['request']['original_utterance'] in DISAGREE:
+            do_any_help(req, res)
+            return
+
+        do_not_understand(req, res)
+        return
 
 
 def start_handler(req, res):
-
     # Проверка авторизованности пользователя
     if (random.random() > 0.7):
-        #отправка сообщения об авторизации
+        # отправка сообщения об авторизации
         do_auth(req, res)
         return
 
@@ -187,7 +227,6 @@ def start_handler(req, res):
     if N == 0:
         # Писем нет
         do_no_mails(req, res)
-        do_any_help(req, res)
         return
 
     if N == 1 and M == 1:
@@ -205,25 +244,58 @@ def start_handler(req, res):
         do_many_senders(req, res)
         return
     else:
-        do_error(req, res)
+        do_error(req, res, str(N) + str(M))
         return
 
 
-def read_message(req, res):
+def prep_read_message(req, res, cont=False):
+    # Подготовка содержания перд отправкой пользователю
 
-    #TODO: content - содержание письма
+    # TODO: content - содержание письма
     content = 'Состояние сессии перестанет храниться, если в ответе навыка не вернуть свойство session_state. Поэтому если для конкретного запроса состояние не меняется, но его нужно продолжать хранить, навыку следует вернуть тот же объект session_state, что пришел в запросе.'
     name = 'RandomName'
 
     if len(content.split()) > 30:
-        content = ' '.join(content.split()[:20])
-        do_large_mail(req, res, name, content)
+        if cont:
+            content = ' '.join(content.split()[20:])
+            do_cont_mail(req, res, name, content)
+        else:
+            content = ' '.join(content.split()[:20])
+            do_large_mail(req, res, name, content)
     else:
         do_small_mail(req, res, name, content)
 
 
-def do_error(req, res):
-    text = 'Error!!! '
+def other_mails(req, res):
+    # Проверка есть ли еще непрочитанные письма у пользователя
+
+    if random.random() > 0.5:
+        do_no_more_mails(req, res)
+        return
+    do_any_more_mails(req, res)
+    return
+
+
+def get_number(req):
+    # Получение первого числа из ответа пользователя
+
+    if 'request' not in req:
+        return False
+
+    if 'nlu' not in req['request']:
+        return False
+
+    if 'tokens' not in req['request']['nlu'] or 'entities' not in req['request']['nlu']:
+        return False
+
+    for i in req['request']['nlu']['entities']:
+        if i['type'] == 'YANDEX.NUMBER':
+            return i['value']
+    return False
+
+
+def do_error(req, res, msg):
+    text = 'Error!!! ' + msg
     res['response']['text'] += text
 
 
@@ -236,6 +308,7 @@ def do_not_understand(req, res):
     res['user_state_update'] = temp_state
 
 
+# 0
 def do_auth(req, res):
     # TODO: Здесь должна быть авторизация
 
@@ -244,14 +317,17 @@ def do_auth(req, res):
     res['user_state_update'] = States.AUTH
 
 
+# 1
 def do_no_mails(req, res):
     # У вас нет новых писем
 
     text = 'У вас нет новых писем. '
     res['response']['text'] += text
     res['user_state_update'] = States.NoMAILS
+    do_any_help(req, res)
 
 
+# 2
 def do_one_mail(req, res):
     # У вас 1 новое письмо от Имя с темой тема. Вам прочитать это письмо?
     name = "RandomName"
@@ -262,6 +338,7 @@ def do_one_mail(req, res):
     res['user_state_update'] = States.OneMAIL
 
 
+# 3
 def do_one_sender(req, res):
     # От имя пришло n1 писем с темами: 1. тема1 2. тема2. Назовите номер письма, содержание которого хотите прослушать
     # TODO: числительные
@@ -269,7 +346,7 @@ def do_one_sender(req, res):
     name = "RandomName"
     topics = []
     for i in range(N):
-        topics.append('RandomTopic{}'.format(i+1))
+        topics.append('RandomTopic{}'.format(i + 1))
 
     text = 'От {0} пришло {1} писем с темами: '.format(name, len(topics))
     for i in range(len(topics)):
@@ -279,6 +356,7 @@ def do_one_sender(req, res):
     res['user_state_update'] = States.OneSENDER
 
 
+# 4
 def do_many_senders(req, res):
     # У вас: 1. n1 писем от Имя1 2. n2 писем от Имя2. Темы какого отправителя вы хотите прослушать? Можно назвать порядковый номер отправителя
     # TODO: числительные
@@ -287,28 +365,31 @@ def do_many_senders(req, res):
     names = []
     Ntopics = dict()
     for i in range(M):
-        names.append('RandomName{}'.format(i+1))
-        if i+1 == M:
-            Ntopics['RandomName{}'.format(i+1)] = N - i
+        names.append('RandomName{}'.format(i + 1))
+        if i + 1 == M:
+            Ntopics['RandomName{}'.format(i + 1)] = N - i
         else:
-            Ntopics['RandomName{}'.format(i+1)] = 1
+            Ntopics['RandomName{}'.format(i + 1)] = 1
 
     text = "У вас: "
     for i, name in enumerate(names):
-        text += '{0}. {1} писем от {2}.'.format(i + 1, Ntopics[name], name)
+        text += '{0}. {1} писем от {2}. '.format(i + 1, Ntopics[name], name)
     text += 'Темы какого отправителя вы хотите прослушать? Можно назвать порядковый номер отправителя. '
     res['response']['text'] += text
     res['user_state_update'] = States.ManySENDERS
 
 
+# 5
 def do_small_mail(req, res, name, content):
     # Письмо от Имя: (содержание)
 
     text = 'Письмо от {0}: {1}'.format(name, content)
     res['response']['text'] += text
     res['user_state_update'] = States.SmallMAIL
+    other_mails(req, res)
 
 
+# 6
 def do_large_mail(req, res, name, content):
     # Письмо от Имя: (содержание первые 20 слов). Это были первые 20 слов, дальше продолжать?
     # TODO: числительные
@@ -318,22 +399,27 @@ def do_large_mail(req, res, name, content):
     res['user_state_update'] = States.LargeMAIL
 
 
+# 9
 def do_cont_mail(req, res, name, content):
     # Продолжение письма от имя: (продолжение)
 
     text = 'Продолжение письма от {0}: {1}'.format(name, content)
     res['response']['text'] += text
     res['user_state_update'] = States.ContMAIL
+    other_mails(req, res)
 
 
+# 7
 def do_no_more_mails(req, res):
     # У вас больше нет новых сообщений
 
     text = 'У вас больше нет новых сообщений. '
     res['response']['text'] += text
     res['user_state_update'] = States.NoMoreMAIL
+    do_any_help(req, res)
 
 
+# 8
 def do_any_more_mails(req, res):
     # У вас еще есть непрочитанные сообщения, вы хотите их прослушать?
 
@@ -342,16 +428,10 @@ def do_any_more_mails(req, res):
     res['user_state_update'] = States.AnyMoreMAIL
 
 
+# 10
 def do_any_help(req, res):
     # Я могу вам еще чем-то помочь? Проверить вашу почту еще раз?
 
     text = 'Я могу вам еще чем-то помочь? Проверить вашу почту еще раз? '
     res['response']['text'] += text
     res['user_state_update'] = States.AnyHELP
-
-
-
-
-
-
-
